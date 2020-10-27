86e0175ff782eaae59d2d5c6f870dbb89431ad1c
Yaroslava <72552637+Yyaroslava@users.noreply.github.com>
2020-10-27 21:17:16 +0200

    Add files via upload

---
+++ lab4.1.cpp
@@ -0,0 +1,44 @@
+#include <iostream> 
+#include <cmath> 
+
+using namespace std;
+
+int main()
+{
+    int i, N;
+    double P;
+
+    cout << "N = "; cin >> N;
+    P = 1;
+    i = N;
+    while (i <= 16)
+    {
+        P *= 1. * i * N / (pow(i, 2) + pow(N, 2));
+        i++;
+    }
+    cout << P << endl;
+
+    P = 1;
+    i = N;
+    do
+    {
+        P *= 1. * i * N / (pow(i, 2) + pow(N, 2));
+        i++;
+    } while (i <= 16);
+    cout << P << endl;
+
+    P = 1;
+    for (i = N; i <= 16; i++)
+    {
+        P *= 1. * i * N / (pow(i, 2) + pow(N, 2));
+    }
+    cout << P << endl;
+
+    P = 1;
+    for (i = 16; i >= N; i--)
+    {
+        P *= 1. * i * N / (pow(i, 2) + pow(N, 2));
+    }
+    cout << P << endl;
+    return 0;
+}
